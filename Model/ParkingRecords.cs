ff33d48f379cd3143ecde91a77cf2a85eb88c098
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-02-28 08:45:34 +0300

    Небольшая коректировка

--- Model/ParkingRecords.cs
+++ Model/ParkingRecords.cs
@@ -5,19 +5,12 @@ namespace Web_Parkovka_Project.Model
     public class ParkingRecords
     {
         public int Id { get; set; }
-
         public int CarId { get; set; }
-
         public required Cars Car { get; set; }
-
         public int ParkingPlacesId { get; set; }
-
         public required ParkingPlaces ParkingPlace { get; set; }
-
         public DateTime Entrance { get; set; } = DateTime.Now;
-
         public DateTime Exit { get; set; }
-
         public int Payment { get; set; }
     }
 }
