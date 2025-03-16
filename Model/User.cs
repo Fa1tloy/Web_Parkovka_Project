07701f0e68331a81467cb85d5a36451533ff76dc
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-03-16 22:38:56 +0300

    Добавил пояснения и немного изменить имена переменных в модели User.

--- Model/User.cs
+++ Model/User.cs
@@ -2,15 +2,15 @@
 
 namespace Web_Parkovka_Project.Model
 {
-    public class User
+    public class User ///пользователь, он же владелец транспортного средства
     {
         public int Id { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public string MiddleName { get; set; }
-        public required string Email { get; set; }
-        public required string PhoneNumber { get; set; }
+        public required string Name { get; set; } ///имя пользователя
+        public required string Surname { get; set; } ///фамилия пользователя
+        public string Patronymic { get; set; } ///отчество пользователя
+        public required string Email { get; set; } ///эл.почта пользователя
+        public required string PhoneNumber { get; set; } ///номер телефона пользователя
 
-        public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+        public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>(); ///что каждый пользователь может владеть несколькими транспортными средствами
     }
 }
