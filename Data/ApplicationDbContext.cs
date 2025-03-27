687757b2f64428dd9fcab11ea0de71be6eff8a70
Gavrilchenko-Stepan <atom47228@gmail.com>
2025-03-27 08:57:29 +0300

    Добавил связи между моделями.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -21,5 +21,29 @@ namespace Web_Parkovka_Project.Data
         public DbSet<Vehicle> Vehicle { get; set; }
         public DbSet<ParkingSpot> ParkingSpot { get; set; }
         public DbSet<Reservation> ParkingRecords { get; set; }
+
+        ///связи между моделями
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+            .HasMany(u => u.Vehicles)
+            .WithOne(v => v.Owner)
+            .HasForeignKey(v => v.OwnerId);
+
+            modelBuilder.Entity<Vehicle>()
+            .HasMany(v => v.ParkedAtSpots)
+            .WithOne(s => s.OccupiedBy)
+            .HasForeignKey(s => s.VehicleId);
+
+            modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.ReservedVehicle)
+            .WithMany()
+            .HasForeignKey(r => r.VehicleId);
+
+            modelBuilder.Entity<Reservation>()
+            .HasOne(r => r.ReservedSpot)
+            .WithMany()
+            .HasForeignKey(r => r.SpotId);
+        }
     }
 }
