84b3c9ee6801388c4fe1a7e6f31e7f75770cd9d1
Fa1tloy <baranovdaniil534@gmail.com>
2025-03-27 09:16:28 +0300

    изменил модель для дольнейшеего изменения бд

--- Model/Reservation.cs
+++ Model/Reservation.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public required DateTime StartDate { get; set; } ///дата и время въезда
-        public required DateTime EndDate { get; set; } ///дата и время выезда
+        public DateTime EndDate { get; set; } ///дата и время выезда
         public int VehicleId { get; set; }
         public Vehicle ReservedVehicle { get; set; } ///информация о транспортном средстве, каждая бронь связана с конкретным транспортным средством
         public int SpotId { get; set; }
--- Model/Vehicle.cs
+++ Model/Vehicle.cs
@@ -6,9 +6,7 @@
         public required string Make { get; set; } ///марка транспортного средства
         public required string Model { get; set; } ///модель транспортного средства
         public required string LicensePlate { get; set; } ///номерной знак транспортного средства
-        public int OwnerId { get; set; }
-        public User Owner { get; set; } ///информация о владельце транспортного средства
-
-        public ICollection<ParkingSpot> ParkedAtSpots { get; set; } = new List<ParkingSpot>(); ///каждое транспортное средство может быть припарковано на нескольких парковочных местах одновременно
+        
+       
     }
 }
