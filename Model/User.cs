677b5d6c8f42be12412a8668dc30084a11004ff4
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-04-03 09:02:27 +0300

    Обновил Validation.

--- Model/User.cs
+++ Model/User.cs
@@ -5,10 +5,15 @@ namespace Web_Parkovka_Project.Model
     public class User ///пользователь, он же владелец транспортного средства
     {
         public int Id { get; set; }
+        [StringLength(50)]
         public required string Name { get; set; } ///имя пользователя
+        [StringLength(50)]
         public required string Surname { get; set; } ///фамилия пользователя
+        [StringLength(50)]
         public string Patronymic { get; set; } ///отчество пользователя
+        [EmailAddress]
         public string Email { get; set; } ///эл.почта пользователя
+        [Phone]
         public string PhoneNumber { get; set; } ///номер телефона пользователя
 
         public List<Vehicle> Vehicles { get; set; } = new List<Vehicle>(); ///что каждый пользователь может владеть несколькими транспортными средствами
--- Model/Vehicle.cs
+++ Model/Vehicle.cs
@@ -1,10 +1,18 @@
-﻿namespace Web_Parkovka_Project.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Parkovka_Project.Model
 {
     public class Vehicle ///транспортное средство
     {
         public int Id { get; set; }
+
+        [StringLength(100)]
         public required string Make { get; set; } ///марка транспортного средства
+
+        [StringLength(100)]
         public required string Model { get; set; } ///модель транспортного средства
+
+        [RegularExpression(@"^[A-Z]\d{5}$")]
         public required string LicensePlate { get; set; } ///номерной знак транспортного средства
         public int OwnerId { get; set; }
         public User Owner { get; set; } ///информация о владельце транспортного средства
