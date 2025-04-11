cdd88e69da7efc6bd8a21782040b7d774c2ad9c6
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-04-11 17:56:58 +0300

    Обновил валидацию, доавив сообщение об обязательном заполнее поля и ошибку, если нарушен диапазон или формат вводимой информации

--- Model/ParkingSpot.cs
+++ Model/ParkingSpot.cs
@@ -6,7 +6,8 @@ namespace Web_Parkovka_Project.Model
     {
         public int Id { get; set; }
 
-        [Range(1,9999)]
+        [Required(ErrorMessage = "Номер парковочного места обязателен для заполнения.")]
+        [Range(1,9999, ErrorMessage = "Номер парковочного места должен быть в диапазоне от 1 до 9999.")]
         public required int Number { get; set; } ///номер парковочного места
         public bool IsOccupied { get; set; } ///занятость
         public int VehicleId { get; set; }
