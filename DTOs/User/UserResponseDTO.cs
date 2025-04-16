57c5fd1de179513450ab1fd693bbb823008959b4
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-04-16 16:48:33 +0300

    Добавил модель UserResponseDTO

---
+++ DTOs/User/UserResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Web_Parkovka_Project.DTOs.User
+{
+    public class UserResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? Patronymic { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FullName => $"{Surname} {Name} {Patronymic}".Trim();
+    }
+}
