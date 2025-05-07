96a4136d0df372e8c6dcf51e0b1a6ec7939ccff5
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-05-07 18:04:16 +0300

    тестовая и профилем который нужно изменять

--- parkovka/Pages/Account/Profile.cshtml.cs
+++ parkovka/Pages/Account/Profile.cshtml.cs
@@ -8,7 +8,6 @@ using Web_Parkovka_Project.Model;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
-
 namespace Web_Parkovka_Project.Pages.Account
 {
     [Authorize]
@@ -27,6 +26,8 @@ namespace Web_Parkovka_Project.Pages.Account
 
         public UserResponseDTO CurrentUser { get; set; }
 
+        [BindProperty]
+        public UpdateProfileDTO Input { get; set; }
         public async Task<IActionResult> OnGetAsync()
         {
             var user = await _userManager.GetUserAsync(User);
@@ -45,7 +46,16 @@ namespace Web_Parkovka_Project.Pages.Account
                 PhoneNumber = user.PhoneNumber
             };
 
-            // «агружаем св€занные транспортные средства
+            Input = new UpdateProfileDTO
+            {
+                Name = user.Name,
+                Surname = user.Surname,
+                Patronymic = user.Patronymic,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            // ????????? ????????? ???????????? ????????
             CurrentUser.Vehicles = await _context.Vehicles
                 .Where(v => v.OwnerId == user.Id)
                 .Select(v => new VehicleDTO
@@ -58,6 +68,39 @@ namespace Web_Parkovka_Project.Pages.Account
                 .ToListAsync();
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                await LoadCurrentUserData();
+                return Page();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Name = Input.Name;
+            user.Surname = Input.Surname;
+            user.Patronymic = Input.Patronymic;
+            user.PhoneNumber = Input.PhoneNumber;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                await LoadCurrentUserData();
+                return Page();
+            }
+
+            return RedirectToPage();
         }
 
         public async Task<IActionResult> OnPostAddVehicleAsync(string make, string model, string licensePlate)
@@ -94,5 +137,28 @@ namespace Web_Parkovka_Project.Pages.Account
             return RedirectToPage();
         }
 
+        private async Task LoadCurrentUserData()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            CurrentUser = new UserResponseDTO
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                Patronymic = user.Patronymic,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Vehicles = await _context.Vehicles
+                    .Where(v => v.OwnerId == user.Id)
+                    .Select(v => new VehicleDTO
+                    {
+                        Id = v.Id,
+                        Make = v.Make,
+                        Model = v.Model,
+                        LicensePlate = v.LicensePlate
+                    })
+                    .ToListAsync()
+            };
+        }
     }
 }
